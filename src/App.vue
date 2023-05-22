3fa3c01fc99573080ef625ea82c020c183b569bd
DavideFarci <yank92.df@gmail.com>
2023-05-22 20:38:50 +0200

    add component for loading page (simulation of loading)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
 import AppList from "./components/AppList.vue";
+import AppLoading from "./components/AppLoading.vue";
 import AppType from "./components/AppType.vue";
 import { store } from "./store";
 import axios from "axios";
@@ -8,17 +9,29 @@ export default {
   data() {
     return {
       store,
+      isLoading: true,
     };
   },
   components: {
     AppType,
     AppList,
     AppHeader,
+    AppLoading,
+  },
+  methods: {
+    loadData() {
+      axios
+        .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0")
+        .then((response) => (this.store.AppList = response.data.data));
+    },
   },
   created() {
-    axios
-      .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0")
-      .then((response) => (this.store.AppList = response.data.data));
+    setTimeout(() => {
+      this.loadData();
+    }, 2000);
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
   },
 };
 </script>
@@ -33,6 +46,7 @@ export default {
     <div class="cards_list">
       <!-- componente Lista con dentro componente card e founded  -->
       <AppList />
+      <AppLoading v-if="isLoading" />
     </div>
   </main>
 </template>
@@ -45,6 +59,7 @@ export default {
 }
 main {
   background-color: #d48f38;
+  padding-bottom: 3rem;
   .cards_list {
     background-color: white;
     max-width: 1000px;
