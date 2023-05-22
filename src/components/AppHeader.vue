cdd468acd5c0695f7b90f8ed5cb595388f01cb88
DavideFarci <yank92.df@gmail.com>
2023-05-22 19:30:47 +0200

    fix header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,11 +4,21 @@ export default {};
 
 <template>
   <header>
-    <div>
-      <img src="" alt="" />
+    <div class="header-container">
+      <img src="../assets/img/Yu-Gi-Oh_Logo.jpg" alt="logo" />
       <h1>Yu-Gi-Oh Api</h1>
     </div>
   </header>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.header-container {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding: 1rem;
+  img {
+    height: 0.8rem;
+  }
+}
+</style>
