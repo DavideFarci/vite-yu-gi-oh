98a55f76d7c4e43546dda6e489ee6e302557f516
DavideFarci <yank92.df@gmail.com>
2023-05-22 20:50:36 +0200

    fix style of select for type

--- src/components/AppType.vue
+++ src/components/AppType.vue
@@ -3,12 +3,26 @@ export default {};
 </script>
 
 <template>
-  <select>
-    <option value="opzione1">Opzione 1</option>
-    <option value="opzione2">Opzione 2</option>
-    <option value="opzione3">Opzione 3</option>
-    <option value="opzione4">Opzione 4</option>
-  </select>
+  <div class="select-container">
+    <select>
+      <option selected>Type</option>
+      <option value="opzione2">Opzione 2</option>
+      <option value="opzione3">Opzione 3</option>
+      <option value="opzione4">Opzione 4</option>
+    </select>
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.select-container {
+  max-width: 1000px;
+  margin: auto;
+  select {
+    padding: 0.5rem;
+    margin: 1rem 0;
+    font-size: 1.1em;
+    font-weight: bold;
+    border-radius: 5px;
+  }
+}
+</style>
